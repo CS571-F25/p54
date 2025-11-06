136faf8fb30d6a493bb57325ff41d7d4a84a87a5
Nate <kavan.nathan@gmail.com>
2025-11-05 19:01:45 -0600

    added back button for about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,12 @@
+import { Link } from 'react-router'
+
 export default function AboutMe(props) {
     return <div>
         <h1>About Us</h1>
+        <Link to="/">
+            <button style={{ padding: '10px 20px', cursor: 'pointer' }}>
+                Back
+            </button>
+        </Link>
     </div>
 }
